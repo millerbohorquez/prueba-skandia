425cb42ecba763986b8d39777f9bfcd05dc6edfc
millerbohorquez <miller-1107@hotmail.com>
2025-09-07 18:59:33 -0500

    style: actualizaciones de estilos y comentarios

--- src/app/component/cards/cards.component.spec.ts
+++ src/app/component/cards/cards.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<<< HEAD:src/app/component/cards/cards.component.spec.ts
 import { CardsComponent } from './cards.component';
 
 describe('CardsComponent', () => {
@@ -14,21 +13,6 @@ describe('CardsComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(CardsComponent);
-========
-import { SlidebarComponent } from './slidebar.component';
-
-describe('SlidebarComponent', () => {
-  let component: SlidebarComponent;
-  let fixture: ComponentFixture<SlidebarComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SlidebarComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(SlidebarComponent);
->>>>>>>> 3a6db11fd64012ac57e18b10fc2475ef21de246a:src/app/component/slidebar/slidebar.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
